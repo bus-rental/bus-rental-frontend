cced2bfcae59a722f07005480d76de7323157926
Simon Egli <simi@simons-macbook-pro-1.home>
2024-10-26 06:20:02 +0200

    add home component

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatIconModule, MatToolbarModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
